2d9481df63736ec0aba5f95e0883a2b012076398
Aryan Raj <adityaraj@Adityas-MacBook-Air.local>
2025-02-19 09:46:49 +0530

    milestone 19 completed

--- frontend/src/pages/productDetails.jsx
+++ frontend/src/pages/productDetails.jsx
@@ -11,7 +11,7 @@ export default function ProductDetails() {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 	const [quantity, setQuantity] = useState(1); // 1. Initialize quantity state
-    const email = "ayan10092018@gmail.com";
+    const email = "araj123@gmail.com";
 
 	useEffect(() => {
 		const fetchProduct = async () => {
